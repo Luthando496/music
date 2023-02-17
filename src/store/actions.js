ce2118da20e5e2149930adefab291a83067513ac
unknown <luthandodidiza197@gmail.com>
2023-02-17 14:07:38 +0200

    used axios

--- src/store/actions.js
+++ src/store/actions.js
@@ -7,11 +7,10 @@ const link = "https://api.deezer.com"
 export const fetchProducts = (id) => {
   return async (dispatch) => {
     try {
-      const info  = await fetch(`/track/${id}`).then(data=> data.json() );
-      // const data = info.json()
+      const {data}  = await axios.get(`/track/${id}`);
       // const data2 = data.json()
-      console.log(info)
-      dispatch(productActions.fetchProducts(info));
+      console.log(data)
+      dispatch(productActions.fetchProducts(data));
     } catch (err) {
       console.log(err);
       dispatch(productActions.fetchProductsError(err.response && err.response.data.message
@@ -26,11 +25,11 @@ export const fetchSingleProduct = (name) => {
   console.log(name)
     return async (dispatch) => {
       try {
-        const data  = await fetch(`/search?q=${name}`).then(data => data.json().catch(err=> console.log(err)))
+        const { data } = await axios.get(`/search?q=${name}`);
         dispatch(productActions.singleFetch(data));
         console.log(data)
       } catch (err) {
-        // console.log(err);
+        console.log(err);
         dispatch(productActions.singleFetchError(err.response && err.response.data.message
           ? err.response.data.message
           : err.message,));
@@ -40,18 +39,18 @@ export const fetchSingleProduct = (name) => {
 
 
 
-// export const searchDrink = (search) => {
-//   return async (dispatch) => {
-//       console.log(search)
-//       try {
-//         const data = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
-//         console.log(data)
-//         dispatch(productActions.fetchProducts(data));
-//       } catch (err) {
-//         console.log(err);
-//         dispatch(productActions.productSearchError(err.response && err.response.data.message
-//           ? err.response.data.message
-//           : err.message,));
-//     }
-//   }
-// }
+export const searchDrink = (search) => {
+  return async (dispatch) => {
+      console.log(search)
+      try {
+        const { data } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+        console.log(data)
+        dispatch(productActions.fetchProducts(data));
+      } catch (err) {
+        console.log(err);
+        dispatch(productActions.productSearchError(err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,));
+    }
+  }
+}
